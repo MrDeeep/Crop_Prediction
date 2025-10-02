120143e17790076daafa5ce858b26e0acca57c1f
Joydip Ram <125433053+MrDeeep@users.noreply.github.com>
2025-10-02 14:44:04 +0530

    Update 1.41 app.py

--- crop_prediction/app.py
+++ crop_prediction/app.py
@@ -4,6 +4,7 @@ from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__, template_folder='website', static_folder='website')
+
 model_path = os.path.join(os.path.dirname(__file__), 'ML_model', 'crop_prediction.pkl')
 with open(model_path, 'rb') as f:
     model = pickle.load(f)
